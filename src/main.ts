630577e00aa475b42cdf69afbf74536611ed2ea3
kxrn0 <kxrp@protonmail.com>
2024-01-05 10:01:25 -0600

    commiting

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 import "reset-css";
+import "./styles/style.scss";
+
+async function fun() {
+  const res = await fetch("/data.json");
+  const json = await res.json();
+
+  console.log(json);
+}
+
+fun();
