c2b4d1575e0bc3ff3f3c2dc4d3bc7c5c50cfbd11
kxrn0 <kxrp@protonmail.com>
2024-01-06 22:00:41 -0600

    been fucking around to find out different effects

---
+++ src/javascripts/api.ts
@@ -0,0 +1,46 @@
+import random from "./random";
+
+type ResponseType = {
+  result: { value: string; message: string };
+  score: number;
+  scores: { reaction: number; memory: number; verbal: number; visual: number };
+};
+
+export default function get_data(): Promise<ResponseType> {
+  return new Promise((resolve) => {
+    const reaction = random(0, 100);
+    const memory = random(0, 100);
+    const verbal = random(0, 100);
+    const visual = random(0, 100);
+    const score = ~~((reaction + memory + verbal + visual) / 4);
+    const result = { value: "", message: "" };
+
+    if (score >= 75) {
+      result.value = "great";
+      result.message =
+        "Your performance exceed 75% of the people conducting the test here!";
+    } else if (score >= 50) {
+      result.value = "average";
+      result.message =
+        "Your performance is average. You are likely a very boring person.";
+    } else if (score >= 25) {
+      result.value = "bad";
+      result.message =
+        "Your performance was below average. Better luck next time!";
+    } else {
+      result.value = "terrible";
+      result.message =
+        "Your performance was terrible. You are a bad person and should feel bad.";
+    }
+
+    setTimeout(
+      () =>
+        resolve({
+          result,
+          score,
+          scores: { reaction, memory, verbal, visual },
+        }),
+      random(1000, 1500)
+    );
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
 import "reset-css";
 import "./styles/style.scss";
+import "./styles/base.scss";
+import "./styles/summary.scss";
+import "./styles/spinner.scss";
+import get_data from "./javascripts/api";
 
-async function fun() {
-  const res = await fetch("/data.json");
-  const json = await res.json();
+const summary = document.querySelector(".summary")!;
+const result: HTMLDivElement = summary.querySelector(".result")!;
+const value: HTMLParagraphElement = result.querySelector(".value")!;
+const message: HTMLParagraphElement = result.querySelector(".message")!;
+const score: HTMLParagraphElement = result.querySelector(".orb .fs-h1")!;
 
-  console.log(json);
+async function get_result() {
+  const res = await get_data();
+
+  value.innerText = res.result.value;
+  message.innerText = res.result.message;
+  summary.classList.add("visible");
+
+  // result.classList.add(res.result.value);
+  result.classList.add("average");
+
+  setTimeout(() => {
+    let count = 0;
+    const interval = setInterval(() => {
+      if (count === res.score) return clearInterval(interval);
+
+      score.innerText = `${count++}`.padStart(2, "0");
+    }, 10);
+  }, 330);
 }
 
-fun();
+get_result();
